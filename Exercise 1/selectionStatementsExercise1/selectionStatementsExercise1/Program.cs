ce88da71e5d6db246a45eb8a551722fbc600e854
calebtripp <calebtripp@outlook.com>
2022-09-14 19:05:20 -0400

    done except while challenge

--- Exercise 1/selectionStatementsExercise1/selectionStatementsExercise1/Program.cs
+++ Exercise 1/selectionStatementsExercise1/selectionStatementsExercise1/Program.cs
@@ -7,23 +7,52 @@
 // The following code will allow the user to guess what your favorite number is:
 Console.WriteLine("Enter a guess for my favorite number:");
 
-var favNum = 2;
 var userInput = int.Parse(Console.ReadLine());
+var favNum = 2;
 
-
- if (userInput < favNum)
+while (userInput != 2)
 {
-    Console.WriteLine("Too low");
+    if (userInput < favNum)
+    {
+        Console.WriteLine("Too low");
+    }
+    else if (userInput > favNum)
+    {
+        Console.WriteLine("Too high");
+    }
+    else if (userInput == favNum)
+    {
+        Console.WriteLine("You guessed it!");
+    }
+    else if (userInput != 2)
+    { 
+    Console.WriteLine("Keep Trying!");
+    int.Parse(Console.ReadLine());
+    }
 }
-else if (userInput > favNum)
-{
-    Console.WriteLine("Too high");
-}
-else if (userInput == favNum)
-{
-    Console.WriteLine("You guessed it!");
-}
+//while (userInput != favNum)
+//{
+//    Console.WriteLine("Keep Trying!");
+//    int.Parse(Console.ReadLine());
+//}
 
 
- //Bonus - make it in a while loop. 
+
+//if (userInput < favNum)
+//{
+//    Console.WriteLine("Too low");
+//}
+//else if (userInput > favNum)
+//{
+//    Console.WriteLine("Too high");
+//}
+//else if (userInput == favNum)
+//{
+//    Console.WriteLine("You guessed it!");
+//}
+
+
+
+
+//Bonus - make it in a while loop. 
 
--- Exercise 2/selectionStatementsExercise2/selectionStatementsExercise2/Program.cs
+++ Exercise 2/selectionStatementsExercise2/selectionStatementsExercise2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter your favorite school subject");
+var favSubj = Console.ReadLine();
+switch (favSubj)
+{
+	case "Coding": // (favSubj == "Coding");
+		Console.WriteLine($"{favSubj} is Nate's favorite subject too!");
+		break;
+	case "Physics":
+        Console.WriteLine($"{favSubj} is Steph's favorite subject too!");
+		break;
+    case "Ethics":
+        Console.WriteLine($"{favSubj} is Caleb's favorite subject too!");
+        break;
+    case "Theology":
+        Console.WriteLine($"{favSubj} is Len's favorite subject too!");
+        break;
+    case "Metallurgy":
+        Console.WriteLine($"{favSubj} is Clayton's favorite subject too!");
+        break;
+    default:
+		Console.WriteLine($"{favSubj}, Really? You really should have picked a more common subject");
+        break;
+}
